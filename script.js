f15384d9fae14700b4e329e6bac13ac1480fd553
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-13 04:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const panels = document.getElementsByClassName("panel"); // html collection []
+
+let curr = 0;
+for(let i=0 ; i< panels.length ; i++){
+	panels[i].addEventListener("click" , function(e){
+		console.log(curr);
+		if(curr > -1 ){ panels[curr].classList.remove("active");
+		}
+		panels[i].classList.add("active");
+		curr = i;
+	} );
+}
